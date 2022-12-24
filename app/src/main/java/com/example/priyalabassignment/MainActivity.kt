71d2efca3243af632c845d0777793313138cfdef
Siva <sivadotpc@gmail.com>
2022-12-24 23:04:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/priyalabassignment/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.priyalabassignment
+
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.ui.Modifier
+import com.example.priyalabassignment.ui.theme.*
+import com.ramcosta.composedestinations.DestinationsNavHost
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SivaLabAssignmentTheme {
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                    DestinationsNavHost(navGraph = NavGraphs.root)
+                }
+            }
+        }
+    }
+}
